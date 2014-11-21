3db678ea8b942518bc6533aba3d6e6bc676a8403
mitzip <git@mitzip.com>
2014-11-21 02:11:41 -0600

    added autoupdate

--- config.fish
+++ config.fish
@@ -3,12 +3,17 @@ if which byobu-launcher
 end
 
 if test -d ~/.local/bin
-    # If you've got it, use it!
-    set --universal fish_user_paths $fish_user_paths ~/.local/bin
+    if not echo $fish_user_paths | grep "$HOME/\.local/bin" > /dev/null ^ /dev/null
+        # If you've got it, use it!
+        set --universal fish_user_paths $fish_user_paths ~/.local/bin
+    end
 end
 
-cd ~/.config/fish
-if not test (git rev-parse HEAD) = (git ls-remote (git rev-parse --abbrev-ref '@{u}' | sed 's/\// /g') | cut -f1)
-    git pull --rebase --stat origin master
+begin
+    set -l origPWD $PWD
+    cd ~/.config/fish
+    if not test (git rev-parse HEAD) = (git ls-remote --heads (git ls-remote --get-url) (git rev-parse --abbrev-ref '@{u}' | sed 's/origin\///g') | cut -f1)
+        git pull --rebase --stat origin master
+    end
+    cd $origPWD
 end
-cd ~/
