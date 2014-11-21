f87296017ca8930af9a8eac40999219726a5c71b
mitzip <git@mitzip.com>
2014-11-20 23:12:17 -0600

    removed extra quote

--- getgen.fish
+++ getgen.fish
@@ -1,4 +1,4 @@
 function getgen -a ebuild -d 'Get ebuild(s) from gentoo-x86 csv'
-    cvs -d :pserver:anonymous@anoncvs.gentoo.org:/var/cvsroot export -D(date '+%Y-%m-%d')" gentoo-x86/$ebuild
+    cvs -d :pserver:anonymous@anoncvs.gentoo.org:/var/cvsroot export -D(date '+%Y-%m-%d') gentoo-x86/$ebuild
 end
 
