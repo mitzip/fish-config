738e58fb597030e6780da086ed1ef59438a4f0e4
mitzip <git@mitzip.com>
2014-11-21 03:46:58 -0600

    autofish now checks for correct git version

--- functions/autofish.fish
+++ functions/autofish.fish
@@ -1,10 +1,21 @@
 # auto updating for whatever branch your on
 function autofish -d 'Auto update fish config and functions'
-    set origPWD $PWD
-    cd ~/.config/fish
-    if not test (git rev-parse HEAD) = (git ls-remote --heads (git ls-remote --get-url) (git rev-parse --abbrev-ref '@{u}' | sed 's/origin\///g') | cut -f1)
-        eval git pull --rebase --stat (git rev-parse --abbrev-ref '@{u}' | sed 's/\// /g')
+    if which git > /dev/null ^ /dev/null
+        set origPWD $PWD
+        cd ~/.config/fish
+        set gitver (git --version | sed -e 's/^git version //' -e 's/\.//g')
+        if math ".$gitver > .171122" # if uses exit code, so reversed comparison
+            set gitRemoteURL (git config --get remote.origin.url)
+        else
+            set gitRemoteURL (git ls-remote --get-url)
+        end
+        if not test (git rev-parse HEAD) = (git ls-remote --heads $gitRemoteURL (git rev-parse --abbrev-ref '@{u}' | sed 's/origin\///g') | cut -f1)
+            eval git pull --rebase --stat (git rev-parse --abbrev-ref '@{u}' | sed 's/\// /g')
+        end
+        cd $origPWD
+    else
+        echo 'Git required to auto update... bailing.'
+        return 1
     end
-    cd $origPWD
 end
 
