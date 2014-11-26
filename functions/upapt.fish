798eadc920a44ff442dc2eb7c74fea3346ee3244
mitzip <git@mitzip.com>
2014-11-26 01:04:12 -0600

    added some error checking to up* functions

--- functions/upapt.fish
+++ functions/upapt.fish
@@ -1,9 +1,13 @@
 function upapt -d 'Fully update an box using APT package manager'
-    sudo apt-get -y update
-    sudo apt-get -y upgrade
-    sudo apt-get -y dist-upgrade
-    sudo apt-get -y autoremove
-    sudo apt-get -y autoclean
-    sudo apt-get check
+    if which apt-get > /dev/null ^ /dev/null
+        sudo apt-get -y update
+        sudo apt-get -y upgrade
+        sudo apt-get -y dist-upgrade
+        sudo apt-get -y autoremove
+        sudo apt-get -y autoclean
+        sudo apt-get check
+    else
+        echo 'Error: apt-get not found'
+    end
 end
 
--- functions/upworld.fish
+++ functions/upworld.fish
@@ -1,6 +1,12 @@
 function upworld -d 'Fully update a box using the Portage package manager'
-    sudo eix-update; and \
-    sudo emerge -uDvaN --with-bdeps=y --complete-graph=y --keep-going=y --backtrack=100 @world; and \
-    sudo revdep-rebuild; and \
-    sudo emerge --depclean
+    if which emerge
+        if which eix-update
+            sudo eix-update
+        end
+        sudo emerge -uDvaN --with-bdeps=y --complete-graph=y --keep-going=y --backtrack=100 @world; and \
+        sudo revdep-rebuild; and \
+        sudo emerge --depclean
+    else
+        echo 'Error: emerge not found'
+    end
 end
