fb1d10f2ab4983eef70228f2ade87cc7b8056395
mitzip <git@mitzip.com>
2014-12-11 16:29:24 -0600

    conditionally add /usr/local/bin to path

--- config.fish
+++ config.fish
@@ -9,6 +9,13 @@ if test -d ~/.local/bin
     end
 end
 
+if test -d /usr/local/bin
+    if not echo $fish_user_paths | grep "/usr/local/bin" > /dev/null ^ /dev/null
+        # If you've got it, use it!
+        set --universal fish_user_paths $fish_user_paths /usr/local/bin
+    end
+end
+
 if test -e ~/.config/fish/functions/autofish.fish
     autofish
 end
