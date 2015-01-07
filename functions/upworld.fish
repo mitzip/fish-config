89ea096d4b97f296e24885b42cbf4bfffa268c1e
mitzip <git@mitzip.com>
2015-01-07 14:59:11 -0600

    updated upworld to check for readonly portage

--- functions/upworld.fish
+++ functions/upworld.fish
@@ -1,8 +1,19 @@
 function upworld -d 'Fully update a box using the Portage package manager'
-    if which emerge > /dev/null ^ /dev/null
-        if which eix-update > /dev/null ^ /dev/null
-            sudo eix-update
+    if mount | grep -v rw > /dev/null ^ /dev/null
+        if mount | grep -v rw | grep portage > /dev/null ^ /dev/null
+            if which eix-update > /dev/null ^ /dev/null
+                sudo eix-update
+            end
+        else
+            if which eix-sync > /dev/null ^ /dev/null
+                sudo eix-sync
+            else
+                sudo emerge --sync
+            end
         end
+    end
+
+    if which emerge > /dev/null ^ /dev/null
         sudo emerge -uDvaN --with-bdeps=y --complete-graph=y --keep-going=y --backtrack=100 @world; and \
         sudo revdep-rebuild; and \
         sudo emerge --depclean
